0c220cde80ed864571ca160c37c5b00d4ee66dba
Yawar Amin <yawar.amin@gmail.com>
2017-04-02 13:06:00 -0400

    Implement skeleton frontend in hyperapp

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,13 @@
 const { h, app } = hyperapp;
 
-app({
-  model: "Hello, World!",
-  view: model => h("p", {}, model)
-});
+const url_base = "localhost:8080";
+const init_model = {
+  forecasts: [], error: {}, select_city: {}, is_loading: false
+};
+
+function view(model, actions) {
+  return h("ul", {},
+    model.forecasts.map(f => h("li", {}, f)));
+}
+
+app({ model: init_model, view });
