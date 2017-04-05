36efa9d7e618cd35cb9057dc0c5680acbef03e40
Yawar Amin <yawar.amin@gmail.com>
2017-04-04 20:20:10 -0400

    WIP: format city forecast

--- frontend/forecast.js
+++ frontend/forecast.js
@@ -1,3 +1,6 @@
+const dateTimeFormat =
+  new Intl.DateTimeFormat("en-CA-u-ca-iso8601", { timeZone: "UTC" });
+
 const Forecast = {
   view: (model, actions) =>
     h("div", { "class": "notification" },
@@ -7,5 +10,12 @@ const Forecast = {
       h("h2", { "class": "subtitle" },
         `# ${model.rank} ${Cities[model.id]}`),
 
-      model.days.map(day => day.dt))
+      h("div", { "class": "tile is-ancestor" },
+        model.days.map(day =>
+          h("div", { "class": "tile is-child notification is-vertical is-2" },
+            h("p", { "class": "is-small" },
+              dateTimeFormat.format(1000 * day.dt)),
+
+            h("p", { "class": "is-small" },
+              `Min: ${day.temp.min}, Max: ${day.temp.max}`)))))
 };
